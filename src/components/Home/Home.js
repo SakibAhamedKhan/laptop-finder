5798e34421a056d67eff7d0047490e160b25e1fd
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-04-05 12:15:53 +0600

    Home part 1 done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,18 @@ import './Home.css'
 const Home = () => {
 	return (
 		<div>
-			<h2>This is Home page</h2>
+			<div className='home'>
+				<div className='intro-container'>
+					<h1>Welcome to the Laptop World</h1>
+					<p>A qualityfull brand have run this store for give the customer better service</p>
+				</div>
+				<div className='laptop-img-container'>
+					<img src="https://www.startech.com.bd/image/cache/catalog/laptop/msi/modern-14-b11mo/modern-14-b11mo-010-500x500.jpg" alt="" />
+				</div>
+			</div>
+			<div className='review-section'>
+				<h1>Customer Reviews(3)</h1>
+			</div>
 		</div>
 	);
 };
