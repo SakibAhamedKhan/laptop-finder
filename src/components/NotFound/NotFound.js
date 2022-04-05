269edea3e127e487e41b7dd530a5939cefb7fc4b
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-04-05 16:09:23 +0600

    NotFound page done

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = () => {
+	return (
+		<div className='notfound'>
+			<img src="https://res.cloudinary.com/practicaldev/image/fetch/s--_GqDpWw0--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/7aqcppklh6bexoa70320.jpg" alt="" />
+		</div>
+	);
+};
+
+export default NotFound;
